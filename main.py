9fb852d4b825ddb7d01c5867d113ff430091bb2f
BeeDeeCommits <bankyabawon@gmail.com>
2023-07-24 23:11:10 -0400

    Added distance_table file and distance_matrix function

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from package_manager import PackageManager
+import csv
+
+
+def distance_matrix():
+    distance_table = []
+    with open('CSV_files/distance_table.csv', 'r') as distance_file:
+        reader = csv.reader(distance_file)
+        for row in reader:
+            adjacency_list = []
+            for column in row:
+                adjacency_list.append(float(column))
+            distance_table.append(adjacency_list)
+    return distance_table
 
 
 def main():
     name = "Bankole Abawonse"
     student_id = "010533366"
     print(f"Name: {name}\nStudent ID: {student_id}")
+    path = 'CSV_files/package_file_CSV.csv'
+    matrix = distance_matrix()
     package_handler = PackageManager()
-    packages = package_handler.read_packages()
-    for package in packages:
-        print(f"{package.package_id}, {package.delivery_status}")
+    packages = package_handler.read_packages(path)
+    print(matrix)
+    """for package in packages:
+        print(f"{package.package_id}, {package.delivery_status}")"""
 
 
 if __name__ == "__main__":
--- package_manager.py
+++ package_manager.py
@@ -7,9 +7,8 @@ class PackageManager:
     def __init__(self):
         self.packages = []
 
-    def read_packages(self):
-        with open('/Users/sirbanks/PycharmProjects/pythonProject/PackageCSV/package_file_CSV.csv', 'r') as package_file:
-            reader = csv.reader(package_file)
+    def read_packages(self, path):
+        with open(path) as package_file:
             for line in package_file:
                 pid, address, state, city, zip_code, deadline, weight = line.split(',')
                 package = Package(pid, address, state, city, zip_code, deadline, weight)
